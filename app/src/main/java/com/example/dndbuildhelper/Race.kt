5764f22e284ebf17a4b0b8c6aa22e954bf4f46f6
ebuntel <buntel.ellery@gmail.com>
2019-04-26 14:29:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/dndbuildhelper/Race.kt
@@ -0,0 +1,12 @@
+package com.example.dndbuildhelper
+
+data class Race(val sName : String,
+                val iSTRChange : Int = 0,
+                val iDEXChange : Int = 0,
+                val iCONChange : Int = 0,
+                val iINTChange : Int = 0,
+                val iCHAChange : Int = 0,
+                val iWISChange : Int = 0)
+{
+
+}
